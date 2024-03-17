b7d6be303a1faba98bb0e204855df333a79dc788
Dmitry <usimovd@gmail.com>
2024-03-17 14:39:46 +0300

    fdsfsd

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,19 @@
 <script>
     let username = '';
+    let isLoggedIn = false;
     
     function displayUsername() {
         if (username.trim() !== "") {
-            alert(`Привет, ${username}!`);
+            isLoggedIn = true;
         }
     }
 </script>
 
-<h1>{username ? `Привет, ${username}!` : 'Войти или зарегистрироваться'}</h1>
-<input type="text" bind:value={username} placeholder="Введите ваш ник">
-<button on:click={displayUsername}>Войти</button>
+{#if isLoggedIn}
+    <h1>Привет, {username}!</h1>
+{:else}
+    <h1>Войти или зарегистрироваться</h1>
+    <input type="text" bind:value={username} placeholder="Введите ваш ник">
+    <button on:click={displayUsername}>Войти</button>
+{/if}
+
