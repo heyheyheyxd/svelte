fc737ec74ee2f515f08859fda52440967e2668fe
Dmitry <usimovd@gmail.com>
2024-03-17 13:23:49 +0300

    fdfd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to home">Heyheyhey shop</a>
+    <div class="nav-links">
+      <a href="/about" class="link">Обо мне</a>
+      <a href="/blog" class="link">Каталог</a>
+      <a href="/contact" class="link">Контакты</a>
+    </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!--Pages will be injected below-->
+    <slot></slot>
+</div>
+
+<style>
+    .container{
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
