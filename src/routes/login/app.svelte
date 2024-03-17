b89422d92f35e9a3b29cc20dbde118da0d4542ae
Dmitry <usimovd@gmail.com>
2024-03-17 14:37:14 +0300

    fdafds

---
+++ src/routes/login/app.svelte
@@ -0,0 +1,13 @@
+<script>
+    let username = '';
+    
+    function displayUsername() {
+        if (username.trim() !== "") {
+            alert(`Привет, ${username}!`);
+        }
+    }
+</script>
+
+<h1>{username ? `Привет, ${username}!` : 'Войти или зарегистрироваться'}</h1>
+<input type="text" bind:value={username} placeholder="Введите ваш ник">
+<button on:click={displayUsername}>Войти</button>
