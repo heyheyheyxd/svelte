437617bb436a179d76ad59b4a4d53c8dba3fb2fc
Dmitry <usimovd@gmail.com>
2024-03-17 14:49:13 +0300

    fdfds

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,26 +1,21 @@
-<!-- LoginPage.svelte -->
 <script>
     let username = '';
-    let isLoggedIn = false;
+    let message = 'Введите свой ник';
 
-    function login() {
+    function setUsername() {
         if (username.trim() !== "") {
-            isLoggedIn = true;
+            message = `Привет, ${username}!`;
         }
     }
 </script>
 
-{#if isLoggedIn}
-    <div>
-        <h1>Добро пожаловать, {username}!</h1>
-    </div>
-{:else}
-    <div>
-        <h1>Войдите, чтобы продолжить</h1>
-        <input type="text" bind:value={username} placeholder="Введите ваше имя">
-        <button on:click={login}>Войти</button>
-    </div>
-{/if}
+<div>
+    <h1>{message}</h1>
+    {#if message === 'Введите свой ник'}
+        <input type="text" bind:value={username} placeholder="Введите ваш ник" on:keyup={setUsername}>
+    {/if}
+</div>
+
 
 
 
