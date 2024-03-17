b6a5b8ecc8c4e85c040277c66648af468b5f0193
Dmitry <usimovd@gmail.com>
2024-03-17 14:47:13 +0300

    dsafsd

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,36 +1,18 @@
+<!-- LoginPage.svelte -->
 <script>
-    import { onMount } from 'svelte';
-
     let username = '';
     let isLoggedIn = false;
-
-    // Проверяем, сохранено ли имя пользователя при загрузке страницы
-    onMount(() => {
-        const savedUsername = localStorage.getItem('username');
-        if (savedUsername) {
-            username = savedUsername;
-            isLoggedIn = true;
-        }
-    });
 
     function login() {
         if (username.trim() !== "") {
             isLoggedIn = true;
-            localStorage.setItem('username', username); // Сохраняем имя пользователя в локальное хранилище
         }
-    }
-
-    function logout() {
-        isLoggedIn = false;
-        username = '';
-        localStorage.removeItem('username'); // Удаляем имя пользователя из локального хранилища
     }
 </script>
 
 {#if isLoggedIn}
     <div>
         <h1>Добро пожаловать, {username}!</h1>
-        <button on:click={logout}>Выйти</button>
     </div>
 {:else}
     <div>
@@ -39,6 +21,7 @@
         <button on:click={login}>Войти</button>
     </div>
 {/if}
+
 
 
 
